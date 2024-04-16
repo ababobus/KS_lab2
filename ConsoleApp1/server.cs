b4fffeb32cccf854919c9151c4f40f492be2e8b2
vmkvs@ugatu.su <vmkvs@ugatu.su>
2024-04-16 15:28:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server {
+    class Program {
+        static void Main() {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994));
+            serverSocket.Listen(10);
+
+            Console.WriteLine("Появление соединения");
+            while (true) {
+                Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine("подключился клиент: " + clientSocket.RemoteEndPoint);
+
+                Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                clientThread.Start();
+            }
+        }
+
+        static void HandleClient(Socket clientSocket) {
+            try {
+                byte[] buffer = new byte[1024];
+                int receivedBytes = clientSocket.Receive(buffer);
+                string receivedMessage = Encoding.Default.GetString(buffer, 0, receivedBytes);
+                Console.WriteLine("получено от клиента " + clientSocket.RemoteEndPoint + ": " + receivedMessage);
+
+                string responseMessage = "HEllo client";
+                byte[] responseBuffer = Encoding.Default.GetBytes(responseMessage);
+                clientSocket.Send(responseBuffer);
+            } catch (Exception ex) {
+                Console.WriteLine("ошибка подключения клиента: " + ex.Message);
+            } finally {
+                clientSocket.Close();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Reflection.PortableExecutable;
+
+namespace Client {
+    class Program {
+        static void Main() {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994);
+            sck.Connect(endpoint);
+
+            Console.Write("введите сообщение");
+            string msg = Console.ReadLine();
+            byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+            sck.Send(msgBuffer, 0, msgBuffer.Length, 0);
+
+            byte[] buffer = new byte[1024];
+            int rec = sck.Receive(buffer, 0, buffer.Length, 0);
+
+            Array.Resize(ref buffer, rec);
+
+            Console.WriteLine("получено: {0}", Encoding.Default.GetString(buffer));
+
+            Console.Read();
+
+        }
+    }
+}
